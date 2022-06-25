394626bd52f2131f54ee58bd7c740536c77f76a5
Daniel Huri <danielhuri.invtb@gmail.com>
2022-06-25 20:03:59 +0300

    Finished assembling GUI

--- app.py
+++ app.py
@@ -1,51 +1,65 @@
-from doctest import master
 import tkinter as tk
+from PIL import ImageTk, Image
 
+# Window configuration
 window = tk.Tk()
 window.title('Secure Password Generator')
-window.geometry('500x350')
+window.geometry('620x350')
 window.columnconfigure(0, weight=1)
 window.rowconfigure(0, weight=1)
+window.iconbitmap('C:\\Users\\danie\\Projects\\Python\\secure-pass-gui\\favicon.ico')
+
+tkvar = tk.StringVar(window)
 
 # Password Options menu
-frame1 = tk.Frame(master=window, borderwidth=1)
-frame1.grid(row=0, column=0, padx=50, pady=100, sticky='w')
-label1 = tk.Label(master=frame1, text='Password Options')
-label1.grid(padx=0, pady=10, sticky='nw')
+option_frame = tk.Frame(master=window, borderwidth=1)
+option_frame.grid(row=0, column=0, padx=50, sticky='w')
+options_label = tk.Label(master=option_frame, text='Password Options')
+options_label.grid(padx=0, pady=10, sticky='w')
 
-symbols_desc = tk.Label(master=frame1, text='Include Symbols:')
+symbols_desc = tk.Checkbutton(master=option_frame, text='Include Symbols')
 symbols_desc.grid(row=1, column=0, sticky='w')
 
-numbers_desc = tk.Label(master=frame1, text='Include Numbers:')
+numbers_desc = tk.Checkbutton(master=option_frame, text='Include Numbers')
 numbers_desc.grid(row=2, column=0, sticky='w')
 
-symbols_desc = tk.Label(master=frame1, text='Include Uppercase Letters:')
+symbols_desc = tk.Checkbutton(master=option_frame, text='Include Uppercase Letters')
 symbols_desc.grid(row=3, column=0, sticky='w')
 
-symbols_desc = tk.Label(master=frame1, text='Include Lowercase Letters:')
+symbols_desc = tk.Checkbutton(master=option_frame, text='Include Lowercase Letters')
 symbols_desc.grid(row=4, column=0, sticky='w')
 
-pass_len = tk.Label(master=frame1, text='Password Length:')
-pass_len.grid(row=5, column=0, sticky='w')
+# Add lock image
+lock_image = Image.open('C:\\Users\\danie\\Projects\\Python\\secure-pass-gui\\padlock.png')
+lock_tk = ImageTk.PhotoImage(lock_image)
 
+lock_label = tk.Label(image=lock_tk)
+lock_label.image = lock_tk
+lock_label.place(x=350, y=15)
 
-frame2 = tk.Frame(master=window, borderwidth=1)
-frame2.grid(row=0, column=1, padx=50, pady= 100, sticky='e')
-label2 = tk.Label(master=frame2)
-label2.grid(padx=0, pady=10, sticky='nw')
+# Create dropdown menu
+numbers = list(range(20, 129)) 
+tkvar.set(20) # Set default value
 
-symbols_check = tk.Checkbutton(master=frame2)
-symbols_check.grid(row=1, column=0, sticky='w')
+pop_menu = tk.OptionMenu(option_frame, tkvar, *numbers)
+length_label = tk.Label(master=option_frame, text='Password Length')
+length_label.grid(row=5, column=0, sticky='w')
+pop_menu.grid(row=5, column=1)
 
-numbers_check = tk.Checkbutton(master=frame2)
-numbers_check.grid(row=2, column=0, sticky='w')
+# Password field
+gen_frame = tk.Frame(master=window, borderwidth=1)
+gen_frame.grid(row=1, column=0, padx=20, sticky='w')
 
-upper_check = tk.Checkbutton(master=frame2)
-upper_check.grid(row=3, column=0, sticky='w')
+# Buttons
+gen_button = tk.Button(master=gen_frame, text='Generate')
+copy_button = tk.Button(master=gen_frame, text='Copy to Clipboard')
+export_button = tk.Button(master=gen_frame, text='Export to File')
 
-lower_check = tk.Checkbutton(master=frame2)
-lower_check.grid(row=4, column=0, sticky='w')
+gen_button.grid(row=2, column=1, sticky='w')
+copy_button.grid(row=2, column=2, sticky='w')
+export_button.grid(row=2, column=3, sticky='w')
 
-
+field_entry = tk.Entry(master=gen_frame, width=50)
+field_entry.grid(row=2, column=0, padx=10, pady=20)
 
 window.mainloop()
